84988e6b39b16b03962d2b5e3d9fbf31721bb76c
Mezai <johan.tedenmark@hotmail.se>
2026-09-12 23:19:40 +0200

    use BernskioldMedia\Fortnox\FortnoxClient;

--- src/Providers/FortnoxServiceProvider.php
+++ src/Providers/FortnoxServiceProvider.php
@@ -10,6 +10,7 @@ use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Support\Arr;
 use Laravel\Socialite\Contracts\Factory;
 use Laravel\Socialite\Facades\Socialite;
+use BernskioldMedia\Fortnox\FortnoxClient;
 use RateLimiter;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
